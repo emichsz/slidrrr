600aeedb893e57c6c605ed00222f543ee27c2c89
Szabolcs Emich <szabolcs@emich.hu>
2013-01-05 23:14:27 +0000

    deploy setup

---
+++ admin/github.config.php
@@ -0,0 +1,24 @@
+<?php
+
+// Enable Deploy Process
+$enabled=true;
+
+// Email Recipient
+$to_email="szabolcs@emich.hu";
+
+// Address Email will come from
+$from_email="szabolcs@emich.hu";
+
+// Site URL being deployed
+$site_name="slidrrr.com";
+
+// Whitelist Users?  (set valid_users if true)
+$limit_users=false;
+
+//////////////////////////
+// Optional
+//////////////////////////
+
+//$valid_users = array();
+
+?>
