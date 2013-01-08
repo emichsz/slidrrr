d90fe82f97efb2622a044434c708de11ea39bf1f
Szabolcs Emich <szabolcs@emich.hu>
2013-01-08 02:59:26 +0100

    Insert GA

--- demo/index.php
+++ demo/index.php
@@ -60,5 +60,18 @@ $(function () {
 <body>
 	<div id="header"><h1>Slidrrr</h1></div>
 	<div id="container"></div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37492416-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
